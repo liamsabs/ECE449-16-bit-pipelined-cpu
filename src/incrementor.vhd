2ea3864f50c61a5ef8ac3e54f450e7a628206640
Aila <14950228+TechGal@users.noreply.github.com>
2024-02-25 21:53:07 -0800

    Commenting

--- src/incrementor.vhd
+++ src/incrementor.vhd
@@ -18,16 +18,16 @@ architecture behavioral of incrementor is
 
 begin
     with control_inc select
-        I1  <=  4 when "00",            -- Normal increment
-            0 when "01",                -- NOP
-            displacement when "10",     -- Displacement
-            0 when others;
+        I1  <=  "0000000000000100"  when "00",      -- Normal increment
+                "0000000000000000"  when "01",      -- NOP
+                displacement        when "10",      -- Displacement
+                "0000000000000000"  when others;    -- Everything Else
 
     with control_prev select
-        I2 <= PC_prev when "0",
-            branch_address when "1";
+        I2 <=   PC_prev         when "0",       -- Add to previous PC
+                branch_address  when "1";       -- Add to branch destination
         
     PC <= I1 + I2;
-    fetch_mem <= I2;
+    fetch_mem <= I2;    -- Is returned value an address to fetch for branch location
 
 end behavioral;
