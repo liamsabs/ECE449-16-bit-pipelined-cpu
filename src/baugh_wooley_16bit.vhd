94c8182364d304f4ac8aaa9627ba798e46b93b0a
Aila <14950228+TechGal@users.noreply.github.com>
2024-02-26 16:56:53 -0800

    Fixed incrementor to implement 16bit adder

--- src/baugh_wooley_16bit.vhd
+++ src/baugh_wooley_16bit.vhd
@@ -13,4 +13,5 @@ entity baugh_wooley_16bit is
 end baugh_wooley_16bit;
 
 architecture functional of baugh_wooley is
+    signal P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11, P12, P13, P15, P16 : std_logic_vector(15 downto 0);
     
