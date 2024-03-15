49f3110028782eae221d30f5fd313271910cd141
Aila <14950228+TechGal@users.noreply.github.com>
2024-03-15 13:01:26 -0700

    Added branching into EX, and updated IF to account for new design

--- src/FETCH.vhd
+++ src/FETCH.vhd
@@ -5,13 +5,12 @@ entity FETCH is
     port(
         Clk             : in std_logic;
         Reset           : in std_logic;
+        PC_reset        : in std_logic;                         -- Resets PC to [val?]
         Br_addr         : in std_logic_vector(15 downto 0);     -- Branch address
         Br_CTRL         : in std_logic;                         -- input signal for PC MUX
-        Br_ZERO         : in std_logic;                         -- receives result from is register zero
-        ID_BR_ZERO      : out std_logic;                        -- tells decoder if branch register was zero 
-        ID_BR_CLRT      : out std_logic;                        -- tells decoder if next instruction is branch
+        IR_in           : in std_logic_vector(15 downto 0);     -- hardcoded Instruction in Value for Format A Test
         IR_out          : out std_logic_vector(15 downto 0);    -- recieved from memory then outputted to IF/ID register
-        IR_in           : in std_logic_vector(15 downto 0)      -- hardcoded Instruction in Value for Format A Test
+        PC_out          : out std_logic_vector(15 downto 0);    -- PC for decoder
     );
 end FETCH;
 
@@ -40,16 +39,18 @@ architecture behavioral of FETCH is
             NPC <= adder_PC;
         else
             NPC <= BR_addr
-       
-        process (Clk, Reset)
+        end if;
+
+        process (Reset)
         begin 
-        if Reset = '1' then -- clear all signals
-            IR_out <= (others => '0');     
-        elsif rising_edge (Clk) then
-        PC <= NPC;
-        IR_out <= IR_sig;
-        ID_BR_ZERO <= BR_ZERO;
-        ID_BR_CTRL <= BR_CTRL;
-        end if; 
+            if Reset = '1' then -- clear all signals
+                IR_out <= (others => '0');     
+            else
+                PC <= NPC;
+                PC_out <= NPC;
+                IR_out <= IR_sig;
+                ID_BR_ZERO <= BR_ZERO;
+                ID_BR_CTRL <= BR_CTRL; 
+            end if;
         end process;            
 end behavioral;
