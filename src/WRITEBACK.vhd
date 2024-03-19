8805c862965fe1acda32242e29a4adb692ada1b0
Aila Simpson <aila@simpsonirving.com>
2024-03-19 15:45:53 -0700

    Update WRITEBACK.vhd
    Only warnings left

--- src/WRITEBACK.vhd
+++ src/WRITEBACK.vhd
@@ -3,7 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity WRITEBACK is
     port (
-        Clk, Reset  : in std_logic;
+        Clk         : in std_logic;
+        WB_Reset    : in std_logic;
         W_data      : in std_logic_vector (15 downto 0);
         W_addr      : in std_logic_vector (2 downto 0);
         W_En        : in std_logic;
@@ -22,9 +23,9 @@ begin
     W_addr_sig <= W_addr;
     W_En_sig <= W_En;
 
-    process (Clk, Reset)
+    process (Clk, WB_Reset)
     begin
-        if Reset = '1' then
+        if WB_Reset = '1' then
             WB_data <= (others => '0');
             WB_addr <= (others => '0');
             WB_En   <= '0';   
