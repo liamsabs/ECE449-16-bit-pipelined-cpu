647ca0bf329e4492f44e44a4332c52eaf7985782
Aila Simpson <ailasimpson@uvic.ca>
2024-04-03 12:21:33 -0700

    fixed forwarding A&B simultaneously, added EX mem to port declaration

--- src/CONTROL.vhd
+++ src/CONTROL.vhd
@@ -138,10 +138,8 @@ architecture behavioral of CONTROL is
         port (
             WB_Reset    : in std_logic;
             W_data      : in std_logic_vector (15 downto 0);
-            MEM_data    : in std_logic_vector (15 downto 0);
             W_addr      : in std_logic_vector (2 downto 0);
             W_En        : in std_logic;
-            L_op        : in std_logic_vector (2 downto 0);
             WB_data     : out std_logic_vector (15 downto 0);
             WB_addr     : out std_logic_vector (2 downto 0);
             WB_En       : out std_logic  
@@ -318,9 +316,7 @@ begin
         FW_A_En   => FW_A_En,
         RB_addr   => ID_B_addr,
         FW_B_data => FW_B_data,
-        FW_B_En   => FW_B_En,
-        L_op      => ID_EX_L_op_In,  
-        L_imm     => ID_EX_L_imm_In      
+        FW_B_En   => FW_B_En  
     );
     
     ExecuteStage : EXECUTE port map (
@@ -343,16 +339,18 @@ begin
         BR_addr_out => EX_MEM_BR_addr_In,
         BR_sub_PC   => ID_EX_BR_sub_PC_Out,
         IN_data     => Input_sig,      
-        IN_En       => ID_EX_IN_En_Out     
+        IN_En       => ID_EX_IN_En_Out,
+        Mem_op_in   => ID_EX_L_op_out,
+        Mem_op_out  => EX_MEM_L_op_in,
+        Mem_imm     => ID_EX_L_imm_In,
+        RB_data_out => EX_MEM_MEM_din_In
     );
     
     WriteBackStage: WRITEBACK port map (
         WB_Reset  => Reset,
-        W_data    => MEM_WB_RW_data_Out,
-        MEM_data  => MEM_WB_MEM_dout_Out,
-        W_addr    => MEM_WB_RW_addr_Out,         
-        W_En      => EX_MEM_RW_En_Out,
-        L_op      => MEM_WB_L_op_Out,            
+        W_data    => EX_MEM_RW_data_Out, 
+        W_addr    => EX_MEM_RW_addr_Out,         
+        W_En      => EX_MEM_RW_En_Out,            
         WB_data   => ID_WB_data,   
         WB_addr   => ID_WB_addr,  
         WB_En     => ID_WB_En      
@@ -364,27 +362,25 @@ begin
    
     FWD : process(Clk, Rst, IR_in_from_TB, EX_MEM_RW_data_In, ID_WB_data, ID_A_addr, ID_B_addr, EX_MEM_RW_addr_In, ID_WB_addr)
     begin        
-        -- Forwarding logic
+        -- Forwarding logic (A)
         if EX_MEM_RW_addr_In = ID_A_addr then
             FW_A_En <= '1';
             FW_A_data <= EX_MEM_RW_data_In;
-        elsif EX_MEM_RW_addr_In = ID_B_addr then
-            FW_B_En <= '1';
-            FW_B_data <= EX_MEM_RW_data_In;
         elsif ID_WB_addr = ID_A_addr then
             FW_A_En <= '1';
             FW_A_data <= ID_WB_data;
+        end if;
+        
+        -- Forwarding logic (B)
+        if EX_MEM_RW_addr_In = ID_B_addr then
+            FW_B_En <= '1';
+            FW_B_data <= EX_MEM_RW_data_In;
         elsif ID_WB_addr = ID_B_addr then
             FW_B_En <= '1';
             FW_B_data <= ID_WB_data;
         end if;
+        
     end process FWD; 
-    
-    MEM : process (EX_MEM_L_op_Out) -- to add Memory stage logic
-    
-    begin
-    
-    end process MEM;
     
     IF_ID : process (Clk, EX_MEM_BR_CTRL_Out, Reset)
     begin
